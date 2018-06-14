9dc11814abdba9381b4e1183e6d5bcffc7ea3b89
npupiec <nkozlowska89@gmail.com>
2018-06-14 15:09:23 +0200

    code for task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports= {
+    'GOOGLE_CLIENT_ID': '974776628829-1l6rvgd2af6uvesfecas53vgcjf1b4me.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECTRET':'f4QlrG0lQCWNt0i4MOm9qM7y',
+    'CALLBACK_URL':'https://localhost:3000/auth/google/callback'
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+const app = express();
+const googleProfile = {};
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('assets'));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+
+    cb(null, profile);
+}
+));
+
+app.get('/', function(req, res){
+    res.render('login-page', { user: req.user });
+});
+
+app.get('/logged', function(req, res){
+    res.render('success', { user: googleProfile });
+});
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+    scope : ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
+
+app.listen(3000);
